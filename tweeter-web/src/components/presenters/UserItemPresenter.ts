28614dac56d5139e6454e37d93330d37e3f4fdf7
AldenKim <aldenkim.ut@gmail.com>
2025-02-02 20:25:19 -0700

    Finished with the video example. Will need to look at instances of FakeData first for a start.

--- tweeter-web/src/components/presenters/UserItemPresenter.ts
+++ tweeter-web/src/components/presenters/UserItemPresenter.ts
@@ -1,11 +1,44 @@
-export interface UserItemView {
+import { AuthToken, User } from "tweeter-shared";
 
+export interface UserItemView {
+  addItems: (newItems: User[]) => void;
+  displayErrorMessage: (message: string) => void;
 }
 
-export class UserItemPresenter {
-    private view: UserItemView;
+export abstract class UserItemPresenter {
+  private _hasMoreItems = true;
+  private _lastItem: User | null = null;
 
-    public constructor(view: UserItemView) {
-        this.view = view;
-    }
+  private _view: UserItemView;
+
+  public constructor(view: UserItemView) {
+    this._view = view;
+  }
+
+  protected get view() {
+    return this._view;
+  }
+
+  public get hasMoreItems() {
+    return this._hasMoreItems;
+  }
+
+  protected set hasMoreItems(value: boolean) {
+    this._hasMoreItems = value;
+  }
+
+  protected get lastItem() {
+    return this._lastItem;
+  }
+
+  protected set lastItem(value: User | null) {
+    this._lastItem = value;
+  }
+
+  reset() {
+    this.lastItem = null;
+    this.hasMoreItems = true;
+  }
+
+  public abstract loadMoreItems(authToken: AuthToken, userAlias: string): void;
 }
