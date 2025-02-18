93680acc69941703aba4470853f33c0106ef1e58
AldenKim <aldenkim.ut@gmail.com>
2025-02-18 12:16:47 -0700

    Adding new files for generics exercise, testing

--- tweeter-web/src/components/presenters/Presenter.ts
+++ tweeter-web/src/components/presenters/Presenter.ts
@@ -2,6 +2,15 @@ export interface View {
   displayErrorMessage: (message: string, bootstrapClasses?: string) => void;
 }
 
+export interface MessageView {
+  displayInfoMessage(
+    message: string,
+    duration: number,
+    bootstrapClasses?: string
+  ): void;
+  clearLastInfoMessage(): void;
+}
+
 export class Presenter {
   private _view: View;
 
@@ -12,4 +21,14 @@ export class Presenter {
   protected get view() {
     return this._view;
   }
+
+   public async doFailureReportingOperation(operation:() => Promise<void>, operationDescription: string) {
+      try {
+        await operation();
+      } catch (error) {
+        this.view.displayErrorMessage(
+          `Failed to ${operationDescription} because of exception: ${error}`
+        );
+      }
+    }
 }
