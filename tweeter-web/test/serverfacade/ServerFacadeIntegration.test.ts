e58efc6b9a9b934277aa86d4fc1f8a417b4c66e9
AldenKim <aldenkim.ut@gmail.com>
2025-03-15 16:34:32 -0600

    writing tests for get followers

---
+++ tweeter-web/test/serverfacade/ServerFacadeIntegration.test.ts
@@ -0,0 +1,25 @@
+import { PagedUserItemRequest } from "tweeter-shared";
+import { ServerFacade } from "../../src/components/network/ServerFacade";
+import 'isomorphic-fetch';
+
+describe("Server Facade Integration Tests", () => {
+  let serverFacade: ServerFacade;
+
+  beforeAll(() => {
+    serverFacade = new ServerFacade();
+  });
+
+  test("Get Followers Integrations Test", async () => {
+    const request: PagedUserItemRequest = {
+      token: "value1",
+      userAlias: "value2",
+      pageSize: 10,
+      lastItem: null,
+    };
+
+    const [followers, hasMore] = await serverFacade.getMoreFollowers(request);
+
+    expect(Array.isArray(followers)).toBe(true);
+    expect(followers.length).toBeGreaterThanOrEqual(0);
+  });
+});
