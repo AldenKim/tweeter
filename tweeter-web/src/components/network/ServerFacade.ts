85238d1aaaaad0a11ff4ef883eebe3334b869deb
AldenKim <aldenkim.ut@gmail.com>
2025-03-16 19:17:52 -0600

    adding story lambda and api

--- tweeter-web/src/components/network/ServerFacade.ts
+++ tweeter-web/src/components/network/ServerFacade.ts
@@ -84,7 +84,33 @@ export class ServerFacade {
     // Handle errors
     if (response.success) {
       if (items == null) {
-        throw new Error(`No followers found`);
+        throw new Error(`No feed items found`);
+      } else {
+        return [items, response.hasMore];
+      }
+    } else {
+      console.error(response);
+      throw new Error(response.message ?? undefined);
+    }
+  }
+
+  public async getMoreStoryItems(
+    request: PagedStatusItemRequest
+  ): Promise<[Status[], boolean]> {
+    const response = await this.clientCommunicator.doPost<
+      PagedStatusItemRequest,
+      PagedStatusItemResponse
+    >(request, "/story/list");
+
+    const items: Status[] | null =
+      response.success && response.items
+        ? response.items.map((dto) => Status.fromDto(dto) as Status)
+        : null;
+
+    // Handle errors
+    if (response.success) {
+      if (items == null) {
+        throw new Error(`No story items found`);
       } else {
         return [items, response.hasMore];
       }
